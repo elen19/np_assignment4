e2d52a70cc570474d165eba58cd79e01c325834d
Elis <elen19@student.bth.se>
2021-03-08 15:44:04 +0100

    En svag början som inte fungerar alls atm. Tog mycket ,inserpation, från tidigare uppgift. Tanken är att de ska bli en UDP

--- client.cpp
+++ client.cpp
@@ -1,1 +1,107 @@
-//clients
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#define DEBUG
+
+void intSignal(int sig)
+{
+    exit(0);
+}
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
+    {
+        printf("Wrong format.\n");
+        exit(0);
+    }
+    char delim[] = ":";
+    char *Desthost = strtok(argv[1], delim);
+    char *Destport = strtok(NULL, delim);
+
+    if (Desthost == NULL || Destport == NULL)
+    {
+        printf("Wrong format\n");
+        exit(0);
+    }
+    int port = atoi(Destport);
+    addrinfo sa, *si, *p;
+    memset(&sa, 0, sizeof(sa));
+    sa.ai_family = AF_INET;
+    sa.ai_socktype = SOCK_DGRAM;
+    sa.ai_protocol = 17;
+    int sockfd;
+    int rv;
+    struct timeval tv;
+    tv.tv_sec = 2;
+    tv.tv_usec = 0;
+    if ((rv = getaddrinfo(Desthost, Destport, &sa, &si)) != 0)
+    {
+        fprintf(stderr, "getadrrinfo: %s\n", gai_strerror(rv));
+    }
+    struct sockaddr_in servaddr;
+    for (p = si; p != NULL; p = p->ai_next)
+    {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+        {
+            printf("Failed to creat socket.\n");
+            continue;
+        }
+        printf("Socket created\n");
+        break;
+    }
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(tv));
+
+    if (p == NULL)
+    {
+        printf("Failed to create socket, p=NULL.\n");
+        freeaddrinfo(si);
+        exit(0);
+    }
+
+    ssize_t sentbytes;
+    msg.type = htons(22);
+    msg.message = htonl(0);
+    msg.protocol = htons(17);
+    msg.major_version = htons(1);
+    msg.minor_version = htons(0);
+
+    printf("Message sent.\n");
+
+    socklen_t addr_len = sizeof(servaddr);
+    int bytes = -1;
+    int tries = 0;
+    signal(SIGINT, intSignal);
+    while (tries < 3 && bytes < 0)
+    {
+        if ((sentbytes = sendto(sockfd, &msg, sizeof(msg), 0, p->ai_addr, p->ai_addrlen)) == -1)
+        {
+            printf("Failed to send via sento function. \n");
+            exit(0);
+        }
+        bytes = recvfrom(sockfd, &protmsg, sizeof(protmsg), 0, (struct sockaddr *)&servaddr, &addr_len);
+        if (bytes == -1 && tries < 2)
+        {
+            printf("Server did not respond... Trying again.\n");
+        }
+        tries++;
+    }
+    if (bytes == -1)
+    {
+        printf("Failed to recive message or server did not respond. Exiting...\n");
+        exit(0);
+    }
+    else
+    {
+        sleep(3);
+    }
+    close(sockfd);
+    return 0;
+}
--- server.cpp
+++ server.cpp
@@ -1,1 +1,119 @@
-//server
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <vector>
+
+#define MENU "Please select:\n1. Play\n2. Watch\n3. Scoreboard\n0. Quit\n"
+
+int terminate = 0;
+struct cli
+{
+  struct sockaddr_in adress;
+  struct timeval tid;
+};
+std::vector<cli> clients;
+void checkJobbList(int signum)
+{
+  struct timeval t;
+  printf("Let me be, I want to sleep.\n");
+  gettimeofday(&t, NULL);
+  for (size_t i = 0; i < clients.size(); i++)
+  {
+    if (t.tv_sec - clients.at(i).tid.tv_sec > 10)
+    {
+      clients.erase(clients.begin() + i);
+      printf("Client slept...\n");
+    }
+  }
+  return;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    printf("Wrong format.\n");
+    exit(0);
+  }
+  char delim[] = ":";
+  char *Desthost = strtok(argv[1], delim);
+  char *Destport = strtok(NULL, delim);
+
+  if (Desthost == NULL || Destport == NULL)
+  {
+    printf("Wrong format\n");
+    exit(0);
+  }
+  addrinfo sa, *si, *p;
+  memset(&sa, 0, sizeof(sa));
+  sa.ai_family = AF_UNSPEC;
+  sa.ai_socktype = SOCK_DGRAM;
+  sa.ai_protocol = 17;
+  int sockfd;
+  int rv;
+  struct timeval tv;
+  tv.tv_sec = 2;
+  tv.tv_usec = 0;
+  if ((rv = getaddrinfo(Desthost, Destport, &sa, &si)) != 0)
+  {
+    fprintf(stderr, "getadrrinfo: %s\n", gai_strerror(rv));
+  }
+  struct sockaddr_in client;
+  socklen_t client_len;
+  for (p = si; p != NULL; p = p->ai_next)
+  {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+    {
+      printf("Failed to creat socket.\n");
+      continue;
+    }
+    if ((bind(sockfd, p->ai_addr, p->ai_addrlen)) != 0)
+    {
+      printf("Failed to bind.\n");
+      close(sockfd);
+      continue;
+    }
+    printf("Socket created\n");
+    break;
+  }
+  setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(tv));
+  if (p == NULL)
+  {
+    printf("Failed to create socket, p=NULL.\n");
+    freeaddrinfo(si);
+    exit(0);
+  }
+
+  /* 
+     Prepare to setup a reoccurring event every 10s. If it_interval, or it_value is omitted, it will be a single alarm 10s after it has been set. 
+  */
+  struct itimerval alarmTime;
+
+  alarmTime.it_interval.tv_sec = 2;
+  alarmTime.it_interval.tv_usec = 2;
+  alarmTime.it_value.tv_sec = 2;
+  alarmTime.it_value.tv_usec = 2;
+
+  /* Regiter a callback function, associated with the SIGALRM signal, which will be raised when the alarm goes of */
+  signal(SIGALRM, checkJobbList);
+  setitimer(ITIMER_REAL, &alarmTime, NULL); // Start/register the alarm.
+  int bytes = -1;
+  client_len = sizeof(client);
+  int clientNr = 0;
+  int currentClient = -1;
+  while (terminate == 0)
+  {
+  
+
+  }
+  close(sockfd);
+  return (0);
+}
